7a6af092b884bc14076681ae80e9c4a0e057e206
Sean Gao <seangao2022@gmail.com>
2023-06-11 12:43:18 -0400

    added solution to flatten the curve challenge

---
+++ edabit-intro/edabit-intro/main.cpp
@@ -0,0 +1,41 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<float> flattenCurve(vector<int> arr) {
+	vector<float> result;
+	float sum = 0;
+	int count = 0;
+	if (!arr.empty()) {
+		for (int i : arr) {
+			sum += i;
+			count++;
+		}
+
+		for (int i = 0; i < count; i++) {
+			result.push_back(round(sum / count * 10) / 10);
+		}
+	}
+
+	return result;
+}
+
+int main() {
+	int input;
+	vector <int> array;
+
+	cout << "Enter list of integers with a letter at the end: ";
+	while (cin >> input) {
+		array.push_back(input);
+	}
+
+	vector<float> result = flattenCurve(array);
+	for (float i : result) {
+		cout << i << " ";
+	}
+	cout << endl;
+
+
+	return 0;
+}
