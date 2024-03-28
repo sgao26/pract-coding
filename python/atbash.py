379cb961c3d623db41089374cb2d69296cecf36e
Sean Gao <seangao2022@gmail.com>
2024-03-27 22:49:41 -0400

    added atbash code to python challenges

---
+++ python/atbash.py
@@ -0,0 +1,23 @@
+# edabit: very hard
+# https://edabit.com/challenge/MGALfBAXhXqqdFyqo
+
+def atbash(txt):
+	mirror = ""
+	for i in txt:
+		if i.isupper():
+			displace = 90 - ord(i)
+			mirror += chr(65 + displace)
+		elif i.islower():
+			displace = 122 - ord(i)
+			mirror += chr(97 + displace)
+		else:
+			mirror += i
+		
+	return mirror
+			
+
+# testing
+print(atbash("abcdefghijklmnopqrstuvwxyz")) # "zyxwvutsrqponmlkjihgfedcba"
+print(atbash("ABCDEFGHIJKLMNOPQRSTUVWXYZ")) # "ZYXWVUTSRQPONMLKJIHGFEDCBA")
+print(atbash("The word 'atbash' derives from the the first and last 2 letters of the Hebrew alphabet.")) # "Gsv dliw 'zgyzhs' wvirevh uiln gsv gsv urihg zmw ozhg 2 ovggvih lu gsv Svyivd zokszyvg."
+print(atbash("Vmxibkgrlm zmw wvxibkgrlm ziv rwvmgrxzo uli gsv Zgyzhs xrksvi.")) # "Encryption and decryption are identical for the Atbash cipher."
