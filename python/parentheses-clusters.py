8ee1bae5619e7cfd76b3fb80db30a4743ee434b4
Sean Gao <seangao2022@gmail.com>
2024-04-02 22:21:50 -0400

    added parentheses cluster code to python challenges

---
+++ python/parentheses-clusters.py
@@ -0,0 +1,31 @@
+# edabit: very hard
+# 6 minutes
+# https://edabit.com/challenge/Fpymv2HieqEd7ptAq
+
+def split(txt):
+	clusters = []
+	balancer = 0
+	cluster = ""
+	for char in txt:
+		if char == '(':
+			balancer += 1
+		else:
+			balancer -= 1
+		cluster += char
+		if balancer == 0:
+			clusters.append(cluster)
+			cluster = ""
+	return clusters
+		
+
+# testing
+print(split("()()()")) # ["()", "()", "()"]
+print(split("")) # []
+print(split("()()(())")) # ["()", "()", "(())"]
+print(split("(())(())")) # ["(())", "(())"]
+print(split("((()))")) # ["((()))"]
+print(split("()(((((((((())))))))))")) # ["()", "(((((((((())))))))))"]
+print(split("((())()(()))(()(())())(()())")) # ["((())()(()))", "(()(())())", "(()())"]
+print(split("((()))(())()()(()())")) # ["((()))", "(())", "()", "()", "(()())"]
+print(split("((())())(()(()()))")) # ["((())())", "(()(()()))"]
+print(split("(()(()()))()(((()))()(()))(()((()))(())())")) # ["(()(()()))", "()", "(((()))()(()))", "(()((()))(())())"]
