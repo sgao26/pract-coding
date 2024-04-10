bf5838fa4cead8fa0389b0a200ca2d396fce278a
Sean Gao <seangao2022@gmail.com>
2024-04-10 16:39:38 -0400

    added simplify fraction code to python challenges

---
+++ python/simplify-fraction.py
@@ -0,0 +1,41 @@
+# edabit: very hard
+# https://edabit.com/challenge/vQgmyjcjMoMu9YGGW
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+def simplify(txt):
+	slashFound = False
+	numer = 0
+	denom = 0
+	numberstr = ""
+	for char in txt:
+		if char == '/':
+			slashFound = True
+			numer = int(numberstr)
+			numberstr = ""
+		else:
+			numberstr += char
+	denom = int(numberstr)
+	divisor = gcd(numer, denom)
+	while divisor != 1:
+		numer = numer/divisor
+		denom = denom/divisor
+		divisor = gcd(numer,denom)
+	if numer % denom == 0:
+		return str(int(numer/denom))
+	else:
+		return (str(int(numer)) + '/' + str(int(denom)))
+	
+# testing
+print(simplify("5/7")) # "5/7"
+print(simplify("4/6")) # "2/3"
+print(simplify("11/10")) # "11/10"
+print(simplify("8/4")) # "2"
+print(simplify("7/4")) # "7/4", 'should work for improper fractions'
+print(simplify("6/4")) # "3/2"
+print(simplify("300/200")) # "3/2"
+print(simplify("50/25")) # "2"
+print(simplify("5/45")) # "1/9"
