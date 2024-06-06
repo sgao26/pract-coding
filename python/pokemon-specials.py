eab833cff95be7bcbfd11c97658b512ea3574ebd
Sean Gao <seangao2022@gmail.com>
2024-06-06 09:45:54 -0400

    added pokemon specials contest code to python challenges

---
+++ python/pokemon-specials.py
@@ -0,0 +1,153 @@
+# edabit: expert
+# https://edabit.com/challenge/KudhhsvJygnEzx8tZ
+
+pk_data = [ { "Number": 1, "Name": "Bulbasaur", "Type1": "grass", "Type2": "poison" }, { "Number": 2, "Name": "Ivysaur", "Type1": "grass", "Type2": "poison" }, { "Number": 3, "Name": "Venusaur", "Type1": "grass", "Type2": "poison" }, { "Number": 4, "Name": "Charmander", "Type1": "fire", "Type2": None }, { "Number": 5, "Name": "Charmeleon", "Type1": "fire", "Type2": None }, { "Number": 6, "Name": "Charizard", "Type1": "fire", "Type2": "flying" }, { "Number": 7, "Name": "Squirtle", "Type1": "water", "Type2": None }, { "Number": 8, "Name": "Wartortle", "Type1": "water", "Type2": None }, { "Number": 9, "Name": "Blastoise", "Type1": "water", "Type2": None }, { "Number": 10, "Name": "Caterpie", "Type1": "bug", "Type2": None }, { "Number": 11, "Name": "Metapod", "Type1": "bug", "Type2": None }, { "Number": 12, "Name": "Butterfree", "Type1": "bug", "Type2": "flying" }, { "Number": 13, "Name": "Weedle", "Type1": "bug", "Type2": "poison" }, { "Number": 14, "Name": "Kakuna", "Type1": "bug", "Type2": "poison" }, { "Number": 15, "Name": "Beedrill", "Type1": "bug", "Type2": "poison" }, { "Number": 16, "Name": "Pidgey", "Type1": "normal", "Type2": "flying" }, { "Number": 17, "Name": "Pidgeotto", "Type1": "normal", "Type2": "flying" }, { "Number": 18, "Name": "Pidgeot", "Type1": "normal", "Type2": "flying" }, { "Number": 19, "Name": "Rattata", "Type1": "normal", "Type2": None }, { "Number": 20, "Name": "Raticate", "Type1": "normal", "Type2": None }, { "Number": 21, "Name": "Spearow", "Type1": "normal", "Type2": "flying" }, { "Number": 22, "Name": "Fearow", "Type1": "normal", "Type2": "flying" }, { "Number": 23, "Name": "Ekans", "Type1": "poison", "Type2": None }, { "Number": 24, "Name": "Arbok", "Type1": "poison", "Type2": None }, { "Number": 25, "Name": "Pikachu", "Type1": "electric", "Type2": None }, { "Number": 26, "Name": "Raichu", "Type1": "electric", "Type2": None }, { "Number": 27, "Name": "Sandshrew", "Type1": "ground", "Type2": None }, { "Number": 28, "Name": "Sandslash", "Type1": "ground", "Type2": None }, { "Number": 29, "Name": "Nidoran", "Type1": "poison", "Type2": None }, { "Number": 30, "Name": "Nidorina", "Type1": "poison", "Type2": None }, { "Number": 31, "Name": "Nidoqueen", "Type1": "poison", "Type2": "ground" }, { "Number": 32, "Name": "Nidoran", "Type1": "poison", "Type2": None }, { "Number": 33, "Name": "Nidorino", "Type1": "poison", "Type2": None }, { "Number": 34, "Name": "Nidoking", "Type1": "poison", "Type2": "ground" }, { "Number": 35, "Name": "Clefairy", "Type1": "normal", "Type2": None }, { "Number": 36, "Name": "Clefable", "Type1": "normal", "Type2": None }, { "Number": 37, "Name": "Vulpix", "Type1": "fire", "Type2": None }, { "Number": 38, "Name": "Ninetales", "Type1": "fire", "Type2": None }, { "Number": 39, "Name": "Jigglypuff", "Type1": "normal", "Type2": None }, { "Number": 40, "Name": "Wigglytuff", "Type1": "normal", "Type2": None }, { "Number": 41, "Name": "Zubat", "Type1": "poison", "Type2": "flying" }, { "Number": 42, "Name": "Golbat", "Type1": "poison", "Type2": "flying" }, { "Number": 43, "Name": "Oddish", "Type1": "grass", "Type2": "poison" }, { "Number": 44, "Name": "Gloom", "Type1": "grass", "Type2": "poison" }, { "Number": 45, "Name": "Vileplume", "Type1": "grass", "Type2": "poison" }, { "Number": 46, "Name": "Paras", "Type1": "bug", "Type2": "grass" }, { "Number": 47, "Name": "Parasect", "Type1": "bug", "Type2": "grass" }, { "Number": 48, "Name": "Venonat", "Type1": "bug", "Type2": "poison" }, { "Number": 49, "Name": "Venomoth", "Type1": "bug", "Type2": "poison" }, { "Number": 50, "Name": "Diglett", "Type1": "ground", "Type2": None }, { "Number": 51, "Name": "Dugtrio", "Type1": "ground", "Type2": None }, { "Number": 52, "Name": "Meowth", "Type1": "normal", "Type2": None }, { "Number": 53, "Name": "Persian", "Type1": "normal", "Type2": None }, { "Number": 54, "Name": "Psyduck", "Type1": "water", "Type2": None }, { "Number": 55, "Name": "Golduck", "Type1": "water", "Type2": None }, { "Number": 56, "Name": "Mankey", "Type1": "fighting", "Type2": None }, { "Number": 57, "Name": "Primeape", "Type1": "fighting", "Type2": None }, { "Number": 58, "Name": "Growlithe", "Type1": "fire", "Type2": None }, { "Number": 59, "Name": "Arcanine", "Type1": "fire", "Type2": None }, { "Number": 60, "Name": "Poliwag", "Type1": "water", "Type2": None }, { "Number": 61, "Name": "Poliwhirl", "Type1": "water", "Type2": None }, { "Number": 62, "Name": "Poliwrath", "Type1": "water", "Type2": "fighting" }, { "Number": 63, "Name": "Abra", "Type1": "psychic", "Type2": None }, { "Number": 64, "Name": "Kadabra", "Type1": "psychic", "Type2": None }, { "Number": 65, "Name": "Alakazam", "Type1": "psychic", "Type2": None }, { "Number": 66, "Name": "Machop", "Type1": "fighting", "Type2": None }, { "Number": 67, "Name": "Machoke", "Type1": "fighting", "Type2": None }, { "Number": 68, "Name": "Machamp", "Type1": "fighting", "Type2": None }, { "Number": 69, "Name": "Bellsprout", "Type1": "grass", "Type2": "poison" }, { "Number": 70, "Name": "Weepinbell", "Type1": "grass", "Type2": "poison" }, { "Number": 71, "Name": "Victreebel", "Type1": "grass", "Type2": "poison" }, { "Number": 72, "Name": "Tentacool", "Type1": "water", "Type2": "poison" }, { "Number": 73, "Name": "Tentacruel", "Type1": "water", "Type2": "poison" }, { "Number": 74, "Name": "Geodude", "Type1": "rock", "Type2": "ground" }, { "Number": 75, "Name": "Graveler", "Type1": "rock", "Type2": "ground" }, { "Number": 76, "Name": "Golem", "Type1": "rock", "Type2": "ground" }, { "Number": 77, "Name": "Ponyta", "Type1": "fire", "Type2": None }, { "Number": 78, "Name": "Rapidash", "Type1": "fire", "Type2": None }, { "Number": 79, "Name": "Slowpoke", "Type1": "water", "Type2": "psychic" }, { "Number": 80, "Name": "Slowbro", "Type1": "water", "Type2": "psychic" }, { "Number": 81, "Name": "Magnemite", "Type1": "electric", "Type2": None }, { "Number": 82, "Name": "Magneton", "Type1": "electric", "Type2": None }, { "Number": 83, "Name": "Farfetchd", "Type1": "normal", "Type2": "flying" }, { "Number": 84, "Name": "Doduo", "Type1": "normal", "Type2": "flying" }, { "Number": 85, "Name": "Dodrio", "Type1": "normal", "Type2": "flying" }, { "Number": 86, "Name": "Seel", "Type1": "water", "Type2": None }, { "Number": 87, "Name": "Dewgong", "Type1": "water", "Type2": "ice" }, { "Number": 88, "Name": "Grimer", "Type1": "poison", "Type2": None }, { "Number": 89, "Name": "Muk", "Type1": "poison", "Type2": None }, { "Number": 90, "Name": "Shellder", "Type1": "water", "Type2": None }, { "Number": 91, "Name": "Cloyster", "Type1": "water", "Type2": "ice" }, { "Number": 92, "Name": "Gastly", "Type1": "ghost", "Type2": "poison" }, { "Number": 93, "Name": "Haunter", "Type1": "ghost", "Type2": "poison" }, { "Number": 94, "Name": "Gengar", "Type1": "ghost", "Type2": "poison" }, { "Number": 95, "Name": "Onix", "Type1": "rock", "Type2": "ground" }, { "Number": 96, "Name": "Drowzee", "Type1": "psychic", "Type2": None }, { "Number": 97, "Name": "Hypno", "Type1": "psychic", "Type2": None }, { "Number": 98, "Name": "Krabby", "Type1": "water", "Type2": None }, { "Number": 99, "Name": "Kingler", "Type1": "water", "Type2": None }, { "Number": 100, "Name": "Voltorb", "Type1": "electric", "Type2": None } ]
+# changed water v water and pyschic v pyschic to 1 instead of 0.5
+type_effectiveness = {'bug': {'bug': 1, 'electric': 1, 'fighting': 0.5, 'fire': 0.5, 'flying': 0.5, 'ghost': 0.5, 'grass': 2, 'ground': 1, 'ice': 1, 'normal': 1, 'poison': 0.5, 'psychic': 2, 'rock': 1, 'water': 1}, 'electric': {'bug': 1, 'electric': 0.5, 'fighting': 1, 'fire': 1, 'flying': 2, 'ghost': 1, 'grass': 0.5, 'ground': 0, 'ice': 1, 'normal': 1, 'poison': 1, 'psychic': 1, 'rock': 1, 'water': 2}, 'fighting': {'bug': 0.5, 'electric': 1, 'fighting': 1, 'fire': 1, 'flying': 0.5, 'ghost': 0, 'grass': 1, 'ground': 1, 'ice': 2, 'normal': 2, 'poison': 0.5, 'psychic': 0.5, 'rock': 2, 'water': 1}, 'fire': {'bug': 2, 'electric': 1, 'fighting': 1, 'fire': 0.5, 'flying': 1, 'ghost': 1, 'grass': 2, 'ground': 1, 'ice': 2, 'normal': 1, 'poison': 1, 'psychic': 1, 'rock': 0.5, 'water': 0.5}, 'flying': {'bug': 2, 'electric': 0.5, 'fighting': 2, 'fire': 1, 'flying': 1, 'ghost': 1, 'grass': 2, 'ground': 1, 'ice': 1, 'normal': 1, 'poison': 1, 'psychic': 1, 'rock': 0.5, 'water': 1}, 'ghost': {'bug': 1, 'electric': 1, 'fighting': 1, 'fire': 1, 'flying': 1, 'ghost': 2, 'grass': 1, 'ground': 1, 'ice': 1, 'normal': 0, 'poison': 1, 'psychic': 2, 'rock': 1, 'water': 1}, 'grass': {'bug': 0.5, 'electric': 1, 'fighting': 1, 'fire': 0.5, 'flying': 0.5, 'ghost': 1, 'grass': 0.5, 'ground': 2, 'ice': 1, 'normal': 1, 'poison': 0.5, 'psychic': 1, 'rock': 2, 'water': 2}, 'ground': {'bug': 0.5, 'electric': 2, 'fighting': 1, 'fire': 2, 'flying': 0, 'ghost': 1, 'grass': 0.5, 'ground': 1, 'ice': 1, 'normal': 1, 'poison': 2, 'psychic': 1, 'rock': 2, 'water': 1}, 'ice': {'bug': 1, 'electric': 1, 'fighting': 1, 'fire': 0.5, 'flying': 2, 'ghost': 1, 'grass': 2, 'ground': 2, 'ice': 0.5, 'normal': 1, 'poison': 1, 'psychic': 1, 'rock': 1, 'water': 0.5}, 'normal': {'bug': 1, 'electric': 1, 'fighting': 1, 'fire': 1, 'flying': 1, 'ghost': 0, 'grass': 1, 'ground': 1, 'ice': 1, 'normal': 1, 'poison': 1, 'psychic': 1, 'rock': 0.5, 'water': 1}, 'poison': {'bug': 1, 'electric': 1, 'fighting': 1, 'fire': 1, 'flying': 1, 'ghost': 0.5, 'grass': 2, 'ground': 0.5, 'ice': 1, 'normal': 1, 'poison': 0.5, 'psychic': 1, 'rock': 0.5, 'water': 1}, 'psychic': {'bug': 1, 'electric': 1, 'fighting': 2, 'fire': 1, 'flying': 1, 'ghost': 1, 'grass': 1, 'ground': 1, 'ice': 1, 'normal': 1, 'poison': 2, 'psychic': 1, 'rock': 1, 'water': 1}, 'rock': {'bug': 2, 'electric': 1, 'fighting': 0.5, 'fire': 2, 'flying': 2, 'ghost': 1, 'grass': 1, 'ground': 0.5, 'ice': 2, 'normal': 1, 'poison': 1, 'psychic': 1, 'rock': 1, 'water': 1}, 'water': {'bug': 1, 'electric': 1, 'fighting': 1, 'fire': 2, 'flying': 1, 'ghost': 1, 'grass': 0.5, 'ground': 2, 'ice': 1, 'normal': 1, 'poison': 1, 'psychic': 1, 'rock': 2, 'water': 1}}
+
+def calc_score(p1,p2):
+	pk_score = float(type_effectiveness[p1["Type1"]][p2["Type1"]])
+	if p2["Type2"]:
+			pk_score *= type_effectiveness[p1["Type1"]][p2["Type2"]]
+	if p1["Type2"]:
+		temp = float(type_effectiveness[p1["Type2"]][p2["Type1"]])
+		if p2["Type2"]:
+			temp *= type_effectiveness[p1["Type2"]][p2["Type2"]]
+		pk_score += temp
+		pk_score /= 2
+	return round(pk_score,3)
+	
+def pk_special_winner(pk1, pk2):
+	pk1_info = {}
+	pk2_info = {}
+	pk1_score = 0
+	pk2_score = 0
+	
+	for info in pk_data:
+		if info["Number"] == pk1:
+			pk1_info = info
+		if info["Number"] == pk2:
+			pk2_info = info
+	
+	pk1_score = calc_score(pk1_info, pk2_info)
+	pk2_score = calc_score(pk2_info, pk1_info)
+	
+	if pk1_score == pk2_score:
+		return -1
+	elif pk1_score > pk2_score:
+		return pk1
+	else:
+		return pk2
+	
+# testing
+# Test cases
+tests = [
+    (4, 14, 4),
+    (71, 54, 71),
+    (43, 44, -1),
+    (83, 33, -1),
+    (41, 45, 41),
+    (5, 80, 80),
+    (92, 51, 51),
+    (27, 12, 12),
+    (18, 72, -1),
+    (17, 7, -1),
+    (68, 6, 6),
+    (60, 97, -1),
+    (63, 74, -1),
+    (81, 30, -1),
+    (49, 37, 37),
+    (24, 76, 76),
+    (91, 82, 82),
+    (34, 68, 34),
+    (63, 51, -1),
+    (90, 17, -1),
+    (61, 65, -1),
+    (31, 40, -1),
+    (20, 3, -1),
+    (58, 69, 58),
+    (87, 65, -1),
+    (92, 47, 92),
+    (71, 66, 71),
+    (9, 62, -1),
+    (85, 17, -1),
+    (1, 33, 33),
+    (12, 13, 12),
+    (52, 9, -1),
+    (87, 14, -1),
+    (33, 28, 28),
+    (77, 52, -1),
+    (19, 78, -1),
+    (24, 89, -1),
+    (72, 99, -1),
+    (77, 18, -1),
+    (25, 44, 44),
+    (57, 51, -1),
+    (60, 22, -1),
+    (36, 65, -1),
+    (98, 34, 98),
+    (26, 12, 26),
+    (51, 56, -1),
+    (59, 94, -1),
+    (70, 44, -1),
+    (67, 13, 13),
+    (31, 33, 31),
+    (37, 85, -1),
+    (3, 86, 3),
+    (96, 71, 96),
+    (93, 34, 34),
+    (63, 99, -1),
+    (69, 65, 65),
+    (41, 8, -1),
+    (13, 53, -1),
+    (84, 35, -1),
+    (73, 70, -1),
+    (84, 70, 84),
+    (72, 14, 72),
+    (74, 22, 74),
+    (61, 90, -1),
+    (6, 46, 6),
+    (58, 77, -1),
+    (39, 46, -1),
+    (79, 20, -1),
+    (72, 68, 72),
+    (80, 98, -1),
+    (42, 97, 97),
+    (26, 19, -1),
+    (65, 35, -1),
+    (12, 65, 12),
+    (35, 49, -1),
+    (58, 89, -1),
+    (18, 67, 18),
+    (75, 69, 69),
+    (2, 4, 4),
+    (83, 49, 83),
+    (11, 57, -1),
+    (56, 87, 56),
+    (33, 17, -1),
+    (79, 96, -1),
+    (33, 4, -1),
+    (80, 77, 80),
+    (52, 57, 57),
+    (68, 26, -1),
+    (9, 38, 9),
+    (99, 42, -1),
+    (61, 86, -1),
+    (51, 22, 22),
+    (21, 77, -1),
+    (32, 26, -1),
+    (73, 47, 73),
+    (29, 92, 92),
+    (7, 37, 7),
+    (12, 63, 12),
+    (53, 88, -1),
+    (34, 60, 60)
+]
+
+for pk1, pk2, expected in tests:
+    result = pk_special_winner(pk1, pk2)
+    if result != expected:
+        print(f"Test failed: pk_special_winner({pk1}, {pk2}) returned {result}, expected {expected}.")
+        break
+else:
+    print("All tests passed!")
