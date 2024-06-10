618833555975ac9ba28c51d10d696d5caf1b7e77
Sean Gao <seangao2022@gmail.com>
2024-06-10 13:54:18 -0400

    added better method to chocolate wrappers challenge in python code

--- python/chocolate-wrappers.py
+++ python/chocolate-wrappers.py
@@ -38,3 +38,17 @@ assert count_eatable_chocolates("0$", "0$") == "Invalid Input", "Test case 6 fai
 assert count_eatable_chocolates("8$", "-2$") == "Invalid Input", "Test case 7 failed"
 
 print("All tests passed")
+
+# better method:
+# from re import findall
+# def count_eatable_chocolates(total_str, cost_str):
+#     total = int(findall(r'-*\d+', total_str)[0])
+#     cost = int(findall(r'-*\d+', cost_str)[0])
+#     if total <= 0 or cost <= 0:
+#         return 'Invalid Input'
+#     count = total // cost
+#     res = count
+#     while count >= 3:
+#         res += count // 3
+#         count = count // 3 + count % 3
+#     return res
