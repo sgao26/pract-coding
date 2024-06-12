1b2149c73f4244b949164d0bc393cc342156ba35
Sean Gao <seangao2022@gmail.com>
2024-06-11 20:37:36 -0400

    added better method to atbash challenge

--- python/atbash.py
+++ python/atbash.py
@@ -21,3 +21,15 @@ print(atbash("abcdefghijklmnopqrstuvwxyz")) # "zyxwvutsrqponmlkjihgfedcba"
 print(atbash("ABCDEFGHIJKLMNOPQRSTUVWXYZ")) # "ZYXWVUTSRQPONMLKJIHGFEDCBA")
 print(atbash("The word 'atbash' derives from the the first and last 2 letters of the Hebrew alphabet.")) # "Gsv dliw 'zgyzhs' wvirevh uiln gsv gsv urihg zmw ozhg 2 ovggvih lu gsv Svyivd zokszyvg."
 print(atbash("Vmxibkgrlm zmw wvxibkgrlm ziv rwvmgrxzo uli gsv Zgyzhs xrksvi.")) # "Encryption and decryption are identical for the Atbash cipher."
+
+# better method
+# def atbash(txt):
+# 	a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+# 	z = 'ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba'
+# 	s = ''
+# 	for i in txt:
+# 		if i in a:
+# 			s += z[a.index(i)]
+# 		else:
+# 			s += i
+# 	return s
