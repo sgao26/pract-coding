c22a2e56498c60892221fae98401b26c8d173fd6
Sean Gao <seangao2022@gmail.com>
2025-01-17 21:52:17 -0500

    added josephus simplified version with different approach

--- python/josephus.py
+++ python/josephus.py
@@ -8,3 +8,16 @@ def who_goes_free(n, k):
 		previous_pos %= len(prisoners)
 		prisoners.pop(previous_pos)
 	return prisoners[0]
+
+#simplified josephus:
+#https://edabit.com/challenge/L9Zh7dWsENnE9P6qc
+
+#formula:
+#j(n)=(j(n-1)+2)%n
+#example: if j(7) is 7, j(8) is j(7)+2 mod 8 which is 7+2 mod 8, or 1
+
+def josephus(people):
+    survivor = 0
+    for i in range(2, people + 1):
+        survivor = (survivor + 2) % i
+    return survivor + 1
